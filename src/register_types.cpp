c8ed78b959d0bd272c3946e0a889f06a35c09e04
ackreika <viniolicar09@gmail.com>
2026-08-25 20:11:19 -0300

    Godot

---
+++ src/register_types.cpp
@@ -0,0 +1,38 @@
+#include "register_types.h"
+#include <gdextension_interface.h>
+#include <godot_cpp/core/defs.hpp>
+#include <godot_cpp/godot.hpp>
+
+// Inclua suas classes aqui (ex: #include "player.h")
+
+using namespace godot;
+
+void initialize_game_module(ModuleInitializationLevel p_level) {
+    if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
+        return;
+    }
+    // Registre suas classes aqui:
+    // ClassDB::register_class<Player>();
+}
+
+void uninitialize_game_module(ModuleInitializationLevel p_level) {
+    if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
+        return;
+    }
+}
+
+extern "C" {
+    GDExtensionBool GDE_EXPORT game_library_init(
+        GDExtensionInterfaceGetProcAddress p_get_proc_address,
+        const GDExtensionClassLibraryPtr p_library,
+        GDExtensionInitialization *r_initialization) {
+
+        godot::GDExtensionBinding::InitObject init_obj(p_get_proc_address, p_library, r_initialization);
+
+        init_obj.register_initializer(initialize_game_module);
+        init_obj.register_terminator(uninitialize_game_module);
+        init_obj.set_minimum_library_initialization_level(MODULE_INITIALIZATION_LEVEL_SCENE);
+
+        return init_obj.init();
+        }
+}
